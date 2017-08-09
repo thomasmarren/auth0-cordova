324d3b51045389bddc12ef18c8939ec047b79dde
Tom Marren <thomasmarren@users.noreply.github.com>
2017-08-09 17:55:13 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ var version = require('./version').raw;
 
 var generateProofKey = crypto.generateProofKey;
 var generateState = crypto.generateState;
-var closingDelayMs = 1000;
+var closingDelayMs = 3000;
 
 session.clean();
 
